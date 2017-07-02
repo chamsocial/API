9cbebf99c7e84b56efdbb442326a0a3560be4f29
Patrik Spathon <patrik@spathon.com>
2017-07-02 14:18:48 +0200

    Create posts graphql query

--- routes/graphql.js
+++ routes/graphql.js
@@ -1,8 +1,33 @@
 const router = require('koa-router')()
 const { graphqlKoa, graphiqlKoa } = require('graphql-server-koa')
 
+const { attributeFields, defaultListArgs, resolver } = require('graphql-sequelize')
+const { GraphQLObjectType, GraphQLList, GraphQLSchema } = require('graphql')
+const { Post } = require('../models')
+
+const types = {
+  post: new GraphQLObjectType({
+    name: 'Post',
+    description: 'A post',
+    fields: attributeFields(Post)
+  })
+}
+
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: 'Queries',
+    fields: {
+      posts: {
+        type: new GraphQLList(types.post),
+        args: defaultListArgs(),
+        resolve: resolver(Post)
+      }
+    }
+  })
+})
+
 router.post('/graphql', graphqlKoa(ctx => ({
-  schema: '',
+  schema,
   context: { user: ctx.user }
 })))
 
