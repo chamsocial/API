50ee5ebcc439b5536f2bc5d0c773a55a696bc19e
Patrik Spathon <patrik@spathon.com>
2018-11-07 20:42:59 +0100

    Fix posts list query

--- routes/graphql/resolvers/queries.js
+++ routes/graphql/resolvers/queries.js
@@ -1,5 +1,16 @@
+const { Post } = require('../../../models')
+
 const queries = {
   me: (_, args, { me }) => me,
+  async posts(_, { limit: limitInput = 10, page = 1 }) {
+    const limit = limitInput < 100 ? limitInput : 100
+    const offset = limitInput * (page - 1)
+    const posts = await Post.findAll({ limit, offset, order: [['created_at', 'DESC']] })
+    return posts
+  },
+  postsInfo: async () => ({
+    count: await Post.count(),
+  }),
 }
 
 module.exports = queries
--- routes/graphql/resolvers/types.js
+++ routes/graphql/resolvers/types.js
@@ -1,3 +1,11 @@
-const types = {}
+const { GraphQLDateTime } = require('graphql-iso-date')
+const { User } = require('../../../models')
+
+const types = {
+  DateTime: GraphQLDateTime,
+  Post: {
+    author: post => User.findById(post.user_id),
+  },
+}
 
 module.exports = types
