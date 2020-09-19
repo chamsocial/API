04e89142680c8e962b1444e2f041707c5a47951d
Patrik Spathon <patrik@spathon.com>
2020-09-19 10:52:46 +0200

    Generate thumbnail

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const fs = require('fs')
 const path = require('path')
 const sharp = require('sharp')
 const router = require('koa-router')()
+const sanitizeFilename = require('sanitize-filename')
 
 const { UPLOADS_DIR } = process.env
 
@@ -45,6 +46,69 @@ router.get('/img/:h/:w/uploads/:userId/:filename', async ctx => {
   } catch (e) {
     ctx.type = 'image/png'
     ctx.body = fs.createReadStream(path.resolve(__dirname, '../public/images/missing.png'))
+  }
+})
+
+
+const isNumber = /^\d*$/
+async function missingImage(ctx, next) {
+  try {
+    await next()
+  } catch (e) {
+    console.log('THUMBNAIL_FAILED', e)
+    if (ctx.get('X-NginX-Proxy')) {
+      console.log('404 Yes')
+      ctx.status = 404
+      ctx.body = ''
+    } else {
+      console.log('404 no')
+      ctx.type = 'image/png'
+      ctx.body = fs.createReadStream(path.resolve(__dirname, '../public/images/missing.png'))
+    }
+  }
+}
+router.get('/thumb/:userId/:h/:w/:filename', missingImage, async ctx => {
+  const {
+    w, h, userId, filename,
+  } = ctx.params
+  const cleanFilename = sanitizeFilename(filename)
+  const ext = path.extname(cleanFilename).replace('.', '').toLowerCase()
+  const mime = mimes[ext]
+  if (
+    !isNumber.test(userId) && !isNumber.test(w) && !isNumber.test(h)
+  ) throw new Error('Has to be numeric')
+  if (!mime) throw new Error('Invalid file format')
+  if (cleanFilename !== filename) throw new Error('Invalid file name')
+  let width = parseInt(w, 10)
+  let height = parseInt(h, 10)
+  if (Number.isNaN(width) || width < 10 || width > 2000) width = 500
+  if (Number.isNaN(height) || height < 10 || height > 2000) height = 500
+
+  const file = path.resolve(UPLOADS_DIR, userId, cleanFilename)
+  await fs.promises.access(file)
+
+  const relThumbPath = path.join(userId, h, w)
+  const absThumbPath = path.join(process.env.THUMBNAIL_DIR, relThumbPath)
+  try {
+    await fs.promises.stat(absThumbPath)
+  } catch (e) {
+    await fs.promises.mkdir(absThumbPath, { recursive: true })
+  }
+
+  console.log('Hello image')
+
+  const absThumbFile = path.join(absThumbPath, cleanFilename)
+  const thumbUrl = path.join(relThumbPath, cleanFilename)
+  ctx.set('X-Accel-Redirect', thumbUrl)
+
+  console.log('Headers', ctx.headers)
+  if (ctx.get('X-NginX-Proxy')) {
+    console.log('Yes')
+    await sharp(file).resize(width, height).toFile(absThumbFile)
+    ctx.body = ''
+  } else {
+    console.log('No')
+    ctx.body = sharp(file).resize(width, height)
   }
 })
 
