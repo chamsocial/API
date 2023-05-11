e53e52f1f6b67ff7b6e79c7a73fa4069035782b0
spathon <patrik@spathon.com>
2023-05-11 20:56:33 +0200

    Redis

--- config/redis.js
+++ config/redis.js
@@ -4,10 +4,5 @@ const port = process.env.REDIS_PORT || process.env.REDIS_SOCK || '/var/run/redis
 const host = process.env.REDIS_HOST || null
 const client = new Redis(port, host)
 
-// Prevent from crashing the app?
-client.on('error', err => {
-  console.log(`Redis error API: ${err}`)
-  throw err
-})
 
 module.exports = client
